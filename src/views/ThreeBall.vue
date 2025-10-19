9ae5b4833b472771470b4a6c8999b1552d97a991
Dggrdg <kobe640670@gmail.com>
2025-10-20 00:01:03 +0800

    20251020  進度

--- src/views/ThreeBall.vue
+++ src/views/ThreeBall.vue
@@ -3,7 +3,6 @@
         <div class="searchArea row">
             <div class="col-2">
                 <select class="form-select" v-model="selectedVendorId">
-                    <option value="" disabled>請選擇廠商</option>
                     <option v-for="vendor in vendorList" :key="vendor.vendorId" :value="vendor.vendorId">
                         {{ vendor.vendorName }}
                     </option>
@@ -11,6 +10,11 @@
             </div>
             <div class="col-1">
                 <button type="button" class="btn btn-warning" @click="exportThreeBall()">匯出報表</button>
+            </div>
+            <div class="col-1">
+                <el-upload class="upload-demo" action="http://localhost:8080/pma/threeBall" accept="xlsx">
+                    <el-button type="primary">點擊上傳</el-button>
+                </el-upload>
             </div>
         </div>
 
@@ -20,8 +24,10 @@
 <script lang="ts" setup>
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
+import { Vendor } from "../Interface/Vendor"
 
-let vendorList = ref("");
+let vendorList = ref<Vendor[]>();
+let selectedVendorId = ref("");
 
 //取得廠商下拉式選單內容
 function getVendor() {
